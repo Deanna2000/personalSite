1892f4b8cf7dfdf95856921979852178e4743bfb
Deanna Vickers <deanna2000@gmail.com>
2018-09-09 02:43:44 -0500

    working on experience section

---
+++ src/containers/Dashboard/Contact/Contact.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+
+class Contact extends Component {
+
+
+	render() {
+
+		return (
+
+			<div className="contactSectionContainer">
+			<h1>Get in Touch!</h1>
+				</div>
+		);
+	}
+}
+
+
+export default Contact;
--- src/containers/Dashboard/Experience/Experience.js
+++ src/containers/Dashboard/Experience/Experience.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Experience.css';
-import profile from '../../../components/IMAGES/deannaProfile.jpg'
+import { Carousel } from 'react-bootstrap';
+// import profile from '../../../components/IMAGES/deannaProfile.jpg'
 
 
 class Experience extends Component {
@@ -11,11 +12,33 @@ class Experience extends Component {
 		return (
 
 			<div>
-					<h1 className="experienceTitle">Experience</h1>
-				<section className="eperienceSectionContainer">
-					<img className="profilepic" alt="deanna profile" src={ profile } />
-				</section>
-			</div>
+				<h1 className="experienceTitle">Experience</h1>
+				<section className="experienceSectionContainer">
+				<div className="experienceItems">
+					<h4>Nashville Software School</h4>
+					<h5>Full Stack Developer</h5>
+					<h6>April - September, 2018</h6>
+					<p>Learned OOP fundamentals through group and individual projects. Created single page applications in Javascript using React.js, CSS3 and Bootstrap. Leveraged Django for creating server-side Python web applications. Used an API with open-source JSON-Server library and used basic SQL commands to create tables and foreign key constraints and to perform CRUD operations. </p>
+					</div>
+					<div className="experienceItems">
+					<h4>Acklen Avenue</h4>
+					<h5>Senior Engagement Manager</h5>
+					<h6>2014 - 2017</h6>
+					<p>Worked with clients as main point of contact on custom software deliveries. Increased client participation and satisfaction by adding designers to our teams and connecting more frequently with clients to confirm design decisions as we built their products. Assisted leadership with internal communications by starting up monthly all hands sessions to review projects, processes, and policies.</p>
+					</div>
+					<div className="experienceItems">
+					<h4>appendTo</h4>
+					<h5>Director of Client Engagement</h5>
+					<h6>2013 - 2014</h6>
+					<p>Transitioned the software delivery team to Agile by adding Scrum practices across all projects. Served as an escalation point for all clients. Provided coaching and mentoring for team members. </p>
+					</div>
+
+					{/* <img className="profilepic" alt="deanna profile" src={profile} /> */}
+				{/* </section> */}
+
+
+</section>
+</div>
 		);
 	}
 }
